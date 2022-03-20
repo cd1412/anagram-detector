b07338cf0a0d7e4d2802a329e4e014d525c60b33
cd1412 <chiduong1412@gmail.com>
2022-03-20 00:06:42 -0500

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select do |item|
+            @word.split("").sort == item.split("").sort
+    end
+end
+end
